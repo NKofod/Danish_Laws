812a999c5d59f673b73b013bdb65414bc25a1897
Nicolai J. Kofod-Jensen <nicolai.kofod.jensen@gmail.com>
2023-03-13 20:20:24 +0100

    Init

---
+++ test.py
@@ -0,0 +1,33 @@
+import requests 
+from bs4 import BeautifulSoup as Soup 
+import time
+import json 
+
+base_url = "https://www.retsinformation.dk/eli/accn/"
+
+with open("log.json","r") as infile: 
+    checked = json.load(infile)
+with open("active.json","r") as infile: 
+    active = json.load(infile)
+    
+
+try: 
+    for level in ["A","B","C"]:
+        for year in range(1900,2024):
+            for num in range(0,10000000):
+                    tmp_url = f"{base_url}{level}{year}{num:07d}/xml"
+                    if tmp_url in checked:
+                        continue 
+                    if num % 10 == 0: 
+                        print(f"{year} - {num}")
+                    tmp_req = requests.get(tmp_url)
+                    tmp_soup = Soup(tmp_req.content)
+                    if "No document found matching ELI request" != tmp_soup.text: 
+                        active.append(tmp_url)
+                        print("FOUND ONE!!!")
+                    checked.append(tmp_url)
+except KeyboardInterrupt:
+    with open("log.json","w") as outfile: 
+        json.dump(checked,outfile,indent=4,ensure_ascii=False)
+    with open("active.json","w") as outfile: 
+        json.dump(active,outfile,indent=4,ensure_ascii=False)
