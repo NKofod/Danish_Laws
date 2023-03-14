f50c455c9a2c88c54487db1689216556a97ba7a9
Nicolai J. Kofod-Jensen <nicolai.kofod.jensen@gmail.com>
2023-03-14 16:15:03 +0100

    Minor commit

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import json
 base_url = "https://www.retsinformation.dk/eli/accn/"
 
 with open("log.json","r") as infile: 
-    checked = json.load(infile)
+    checked = int(infile.read())
 with open("active.json","r") as infile: 
     active = json.load(infile)
 with open("finished_combos.json","r") as infile: 
@@ -15,13 +15,13 @@ count = 0
 
 try: 
     for level in ["A","B","C"]:
+        print(level)
         for year in range(1900,2024):
+            print(year)
             if checked_combos[level][str(year)]:
                 continue 
-            for num in range(0,10000000):
+            for num in range(checked,10000000):
                     tmp_url = f"{base_url}{level}{year}{num:07d}/xml"
-                    if tmp_url in checked:
-                        continue 
                     count += 1
                     if num % 10 == 0: 
                         print(f"{year} - {num}")
@@ -30,18 +30,18 @@ try:
                     if "No document found matching ELI request" != tmp_soup.text: 
                         active.append(tmp_url)
                         print("FOUND ONE!!!")
-                    checked.append(tmp_url)
+                    checked += 1
                     if count == 1000000:
                         raise GeneratorExit
             checked_combos[level][str(year)] = True 
-            checked = []
+            checked = 0
 except KeyboardInterrupt:
     with open("log.json","w") as outfile: 
-        json.dump(checked,outfile,indent=4,ensure_ascii=False)
+        outfile.write(str(checked))
     with open("active.json","w") as outfile: 
         json.dump(active,outfile,indent=4,ensure_ascii=False)
 except GeneratorExit: 
     with open("log.json","w") as outfile: 
-        json.dump(checked,outfile,indent=4,ensure_ascii=False)
+        outfile.write(str(checked))
     with open("active.json","w") as outfile: 
         json.dump(active,outfile,indent=4,ensure_ascii=False)
